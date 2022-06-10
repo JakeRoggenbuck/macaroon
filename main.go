ea8be0969de1b4da6af2072f81fcccfe2271974c
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-06-10 12:10:44 -0700

    Add warn

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/gookit/color"
 )
 
+func warn(message string) {
+	color.Yellow.Print("WARN: ")
+	fmt.Println(message)
+}
+
 func main() {
-	fmt.Println("Hey")
+	var filename string
+	
+	flag.StringVar(&filename, "f", "default", "provide a file to preprocess")
+	flag.Parse()
+
+	if filename == "default" {
+		warn("Please provide a filename")
+		return
+	}
+
+	fmt.Println(filename)
 }
