777827e28908877e895553ca7698933a6c4e1ba0
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-06-10 12:25:57 -0700

    Add args struct

--- main.go
+++ main.go
@@ -6,19 +6,41 @@ import (
 	"os"
 )
 
+func fatal(message string) {
+	color.Yellow.Print("FATAL: ")
+	fmt.Println(message)
+	os.Exit(1)
+}
+
 func warn(message string) {
 	color.Yellow.Print("WARN: ")
 	fmt.Println(message)
 }
 
-func main() {
+type Args struct {
+	filename string
+	hasFilename bool
+}
+
+func parseArgs() Args {
 	argLength := len(os.Args[1:])
 
 	if argLength == 0 {
-		warn("Too few arguments, please provide a filename")
-		return
+		fatal("Too few arguments, please provide a filename")
 	}
 
 	filename := os.Args[1]
-	fmt.Println(filename)
+
+	return Args {
+		filename,
+		true,
+	}
+}
+
+func main() {
+	args := parseArgs()
+
+	if args.hasFilename {
+		fmt.Println(args.filename)
+	}
 }
