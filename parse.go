c9f48ac31f612d17ac23ba8d8882c18dfcaaf189
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-06-10 20:16:24 -0700

    Add actual fine macro parse and lex

--- parse.go
+++ parse.go
@@ -24,14 +24,14 @@ func fast_forward_empty_space(current string) int {
 	return index
 }
 
-func check_start(current string) bool {
+func check_start(current string) int {
 	index := fast_forward_empty_space(current)
 
 	if current[index] == '#' {
-		return true
+		return index
 	}
 
-	return false
+	return -1
 }
 
 type Command int
@@ -41,12 +41,50 @@ const (
 	None
 )
 
-func lex(line string) Command {
+func lex_command(line string) Command {
 	if len(line) < 4 { return None }
 	if line[0:4] == "#set" {
 		return Set
 	}
 	return None 
+}
+
+type Macro struct {
+	command Command
+	name string
+	value string
+}
+
+func tokenize(line string, command Command, start int) Macro {
+	var name string
+	var value string
+	index := start
+	first, second := -1, -1
+
+	for {
+		if index + 1 > len(line) { break; }
+		cur := line[index]
+		if cur == ' ' {
+			// Second value has already been found
+			if second > 0 {
+				break;
+			}
+			
+			// Assign index to either first or second based on if they have been found yet 
+			if first < 0 {
+				first = index
+			} else {
+				second = index
+			}
+		}
+
+		index += 1
+	}
+
+	name = line[first+1:second]
+	value = line[second:]
+
+	return Macro {command, name, value}
 }
 
 func parse(filename string) {
@@ -61,11 +99,11 @@ func parse(filename string) {
 	for scanner.Scan() {
 		current := scanner.Text()
 		if len(current) < 1 { continue; }
-		if status := check_start(current); status {
-			command := lex(current)
-			if command == Set {
-				fmt.Println(current)
-			}
+		if start := check_start(current); start != -1 {
+			command := lex_command(current)
+			if command == None { continue; }
+			macro := tokenize(current, command, start)
+			fmt.Println(macro)
 		}
 	}
 
